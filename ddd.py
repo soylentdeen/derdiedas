d55797c152ceb6be910181d8e119be14a72b6a25
Casey Deen <cdeen@mpe.mpg.de>
2018-04-19 01:43:41 +0200

    added text files

--- ddd.py
+++ ddd.py
@@ -1,4 +1,5 @@
 import numpy
+import glob
 
 class Noun(object ):
     def __init__(self, word, articles=[]):
@@ -13,11 +14,21 @@ class Noun(object ):
                 self.articles[article] += 1
 
 
+    def numCounts(self):
+        num= 0
+        for art in self.articles.keys():
+            num += self.articles[art]
+        return num
+
     def printCounts(self):
+        print("Word: %s" % self.word)
         for art in self.articles.keys():
             if self.articles[art] > 0:
-                print ("%s : %d" % ())
+                print ("%s : %d" % (art, self.articles[art]))
     
+    def guessGender(self):
+        print("")
+
     def __eq__(self, other):
         if self.word == other:
             return True
@@ -42,31 +53,36 @@ articles = ['der', 'die', 'das', 'den', 'dem', 'des', 'einer', 'ein', 'eine', 'e
             'unser', 'unsere', 'unserem', 'unserer', 'unseres', 'unseren',
             'euer', 'euere', 'euerem', 'euerer', 'eueres', 'eueren']
 
-f = open("FAZ.txt", 'r')
+files = glob.glob('*.txt')
 
 library = []
 
-text = f.read()
-endOfSentence = True
-for word in text.split():
-    if word.lower() in articles:
-        currentArticle = word
-    if word[0].isupper() and not(endOfSentence):
-        if (word in library) and (currentArticle != ''):
-            library[library.index(word)].addArticle(currentArticle)
-        print currentArticle, word
-        n = Noun(word)
-        if currentArticle != '':
-            n.addArticle(currentArticle)
-        currentArticle = ''
-        library.append(n)
+currentArticle = ''
+for snippet in files:
+    f = open(snippet, 'r')
+    text = f.read()
+    endOfSentence = True
+    for word in text.split():
+        if word.lower() in articles:
+            currentArticle = word
+        if word[0].isupper() and not(endOfSentence):
+            if (word in library) and (currentArticle != ''):
+                library[library.index(word)].addArticle(currentArticle)
+            n = Noun(word, articles=articles)
+            if currentArticle != '':
+                n.addArticle(currentArticle)
+            currentArticle = ''
+            library.append(n)
 
-    if word[-1] == '.':
-        endOfSentence = True
-    else:
-        endOfSentence = False
+        if word[-1] == '.':
+            endOfSentence = True
+        else:
+            endOfSentence = False
 
-f.close()
+    f.close()
 
 for word in library:
-    print word.articles
+    if word.numCounts() > 1:
+        word.printCounts()
+        word.guessGender()
+        print("")
